2ff6c37c022d632bc98ae89b29f4d7b688b1f78f
weiuou <3159415804@qq.com>
2024-09-29 03:19:40 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 
 export default {
-  base: 'https://raw.githubusercontent.com/weiuou/CF-VP-Helper/refs/heads/gh-pages/',
+  base: '/CF-VP-Helper/',
   root: resolve(__dirname, 'src'),
   build: {
     outDir: '../dist',
